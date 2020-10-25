481e0adca9a529832b6cc55c968fb0d315e6fee3
Eric Kim-Butler <eric.butler@notarize.com>
2020-10-24 20:18:11 -0400

    Add grace Domain Infra

---
+++ domains/grace/infra/main.tf
@@ -0,0 +1,178 @@
+variable "root_domain" {
+  type = string
+}
+
+variable "root_dns_zone_id" {
+  type = string
+}
+
+resource "aws_acm_certificate" "main_cert" {
+  domain_name               = var.root_domain
+  validation_method         = "DNS"
+  subject_alternative_names = ["grace.${var.root_domain}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "main_cert" {
+  certificate_arn         = aws_acm_certificate.main_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.main_cert_validation : record.fqdn]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "grace.${var.root_domain}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = "15"
+    }
+  }
+
+  policy = <<POLICY
+{
+    "Statement": [
+        {
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Principal": "*",
+            "Resource": "arn:aws:s3:::grace.${var.root_domain}/*",
+            "Sid": "PublicReadAccess"
+        }
+    ],
+    "Version": "2012-10-17"
+}
+POLICY
+}
+
+output "static_bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
+}
+
+resource "aws_cloudfront_distribution" "website_cdn" {
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_All"
+  http_version    = "http2"
+
+  origin {
+    origin_id   = "origin-bucket-${aws_s3_bucket.website_bucket.id}"
+    domain_name = aws_s3_bucket.website_bucket.website_endpoint
+
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port              = "80"
+      https_port             = "443"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    min_ttl                = "0"
+    default_ttl            = "86400"    # one day
+    max_ttl                = "31536000" # one year
+    target_origin_id       = "origin-bucket-${aws_s3_bucket.website_bucket.id}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+
+    forwarded_values {
+      query_string = "false"
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.main_cert.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+  aliases = [
+    "grace.${var.root_domain}",
+  ]
+}
+
+output "cdn_id" {
+  value = aws_cloudfront_distribution.website_cdn.id
+}
+
+resource "aws_route53_record" "cdn_alias_a_domain" {
+  zone_id = var.root_dns_zone_id
+  name    = "grace.${var.root_domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cdn_alias_aaaa_domain" {
+  zone_id = var.root_dns_zone_id
+  name    = "grace.${var.root_domain}"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "main_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  zone_id = var.root_dns_zone_id
+  ttl     = 60
+}
--- infra/main.tf
+++ infra/main.tf
@@ -17,9 +17,24 @@ locals {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_route53_zone" "root" {
   name = local.root_domain
+}
+
+module "grace" {
+  source = "../domains/grace/infra"
+
+  root_domain      = local.root_domain
+  root_dns_zone_id = aws_route53_zone.root.zone_id
+}
+
+output "grace_static_bucket_name" {
+  value = module.grace.static_bucket_name
+}
+
+output "grace_cdn_id" {
+  value = module.grace.cdn_id
 }
