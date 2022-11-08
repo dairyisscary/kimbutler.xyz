e497d6781bfc5ec4e01c8614e5aa92b2cd3426ef
Eric Kim-Butler <eric@kimbutler.xyz>
2022-11-08 19:13:20 +0100

    Upgrade www Deps

--- domains/grace/prettier.config.cjs
+++ domains/grace/prettier.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  overrides: [{ files: "*.md", options: { printWidth: 120, proseWrap: "always" } }],
+  overrides: [
+    { files: "*.md", options: { printWidth: 120 } },
+    { files: "*.mdx", options: { printWidth: 120 } },
+  ],
   printWidth: 100,
+  proseWrap: "always",
   trailingComma: "all",
 };
