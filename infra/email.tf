1b9713f04ecafd444fe5a55b8bfd406ac43fb4eb
Eric Kim-Butler <eric@kimbutler.xyz>
2023-02-25 16:06:43 +0100

    Add DKIM SES Verification Records

--- infra/email.tf
+++ infra/email.tf
@@ -27,3 +27,45 @@ resource "cloudflare_record" "hey_mx_email" {
   name     = "@"
   value    = "work-mx.app.hey.com"
 }
+
+resource "cloudflare_record" "aws_ses_dkim_1" {
+  zone_id = cloudflare_zone.root.id
+  type    = "CNAME"
+  name    = "z3i3pfmu2dtqouuammskg26x7dmtvwg2._domainkey.kimbutler.xyz"
+  value   = "z3i3pfmu2dtqouuammskg26x7dmtvwg2.dkim.amazonses.com"
+}
+
+resource "cloudflare_record" "aws_ses_dkim_2" {
+  zone_id = cloudflare_zone.root.id
+  type    = "CNAME"
+  name    = "xcflhuf6od73o7exz4rr5dcnbfrkyeji._domainkey.kimbutler.xyz"
+  value   = "xcflhuf6od73o7exz4rr5dcnbfrkyeji.dkim.amazonses.com"
+}
+
+resource "cloudflare_record" "aws_ses_dkim_3" {
+  zone_id = cloudflare_zone.root.id
+  type    = "CNAME"
+  name    = "c2kpj7rmffsop7u7pz5nmdprejc4nn6k._domainkey.kimbutler.xyz"
+  value   = "c2kpj7rmffsop7u7pz5nmdprejc4nn6k.dkim.amazonses.com"
+}
+
+resource "cloudflare_record" "aws_ses_home_dkim_1" {
+  zone_id = cloudflare_zone.root.id
+  type    = "CNAME"
+  name    = "f45iq4empt6l4pbhsn3y7qsudbxm2wyn._domainkey.home.kimbutler.xyz"
+  value   = "f45iq4empt6l4pbhsn3y7qsudbxm2wyn.dkim.amazonses.com"
+}
+
+resource "cloudflare_record" "aws_ses_home_dkim_2" {
+  zone_id = cloudflare_zone.root.id
+  type    = "CNAME"
+  name    = "2gmhp2fdzrhb7ykkxxb4gad46de22q6g._domainkey.home.kimbutler.xyz"
+  value   = "2gmhp2fdzrhb7ykkxxb4gad46de22q6g.dkim.amazonses.com"
+}
+
+resource "cloudflare_record" "aws_ses_home_dkim_3" {
+  zone_id = cloudflare_zone.root.id
+  type    = "CNAME"
+  name    = "xhdln6bsdm5rcln5a5c5t3imn2znovbk._domainkey.home.kimbutler.xyz"
+  value   = "xhdln6bsdm5rcln5a5c5t3imn2znovbk.dkim.amazonses.com"
+}
