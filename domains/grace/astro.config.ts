72917a70cace98f2caaa00e87e54372a3240c4d4
Eric Kim-Butler <eric@kimbutler.xyz>
2023-06-18 16:13:12 +0200

    Upgrade grace Deps

--- domains/grace/astro.config.ts
+++ domains/grace/astro.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindIntegration from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import minifyHTML from "./minify-html";
-
 export default defineConfig({
-  integrations: [minifyHTML(), tailwindIntegration(), mdx()],
+  compressHTML: true,
+  integrations: [tailwindIntegration(), mdx()],
   site: "https://grace.kimbutler.xyz",
 });
